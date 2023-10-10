6d61fa69b55834b5409229f6124a9277f462fa32
Rafałjgfhjgfjf <rafal,oerfikowski@onet.pl>
2023-10-10 18:09:50 +0200

    WeatherApp

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,7 +2,15 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.weatherapp.databinding.ActivityMainBinding
+import com.example.weatherapp.utils.RetrofitInstance
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +20,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getCurrentWeather()
+    }
+
+    private fun getCurrentWeather() {
+        GlobalScope.launch(Dispatchers.IO) {
+            val response = try {
+                RetrofitInstance.api.getCurrentWeather("new york", "metric", "bd199e6fd842a33e4ae3f454783f53e8")
+            }catch (e: IOException){
+                Toast.makeText(applicationContext, "app error ${e.message}", Toast.LENGTH_SHORT).show()
+                return@launch
+            }catch (e: HttpException){
+                Toast.makeText(applicationContext, "http error ${e.message}", Toast.LENGTH_SHORT).show()
+                return@launch
+            }
+
+            if (response.isSuccessful && response.body()!= null){
+                withContext(Dispatchers.Main){
+                    binding.tvTemp.text = "temp: ${response.body()!!.main.temp}"
+                }
+            }
+
+        }
     }
 }
