c984615d5a1ffa73e7a506702fe94b45648acd80
Rafałjgfhjgfjf <rafal,oerfikowski@onet.pl>
2023-10-12 10:35:28 +0200

    WeatherApp

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.utils.RetrofitInstance
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,7 +41,29 @@ class MainActivity : AppCompatActivity() {
 
             if (response.isSuccessful && response.body()!= null){
                 withContext(Dispatchers.Main){
-                    binding.tvTemp.text = "temp: ${response.body()!!.main.temp}"
+                    val data = response.body()!!
+                    val iconId = data.weather[0].icon
+                    val imgUrl = "https://openweathermap.org/img/w/$iconId.png"
+
+                    Picasso.get()
+                        .load(imgUrl)
+                        .into(binding.imgWeather)
+
+                    binding.tvSunrise.text= SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(data.sys.sunrise * 1000)
+                    binding.tvSunset.text= SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(data.sys.sunset * 1000)
+
+                    binding.apply {
+                        tvStatus.text = data.weather[0].description
+                        tvWind.text = "${data.wind.speed.toString()} KM/h"
+                        tvLocation.text ="${data.name}\n${data.sys.country}"
+                        tvTemp.text ="${data.main.temp.toInt()} °C"
+                        tvFeelsLike.text ="Feels like: ${data.main.feels_like} °C"
+                        tvMinTemp.text ="Min temp: ${data.main.temp_min} °C"
+                        tvMaxTemp.text ="MAx temp: ${data.main.temp_max} °C"
+                        tvMaxTemp.text ="MAx temp: ${data.main.temp_max} °C"
+                        tvPressure.text =" ${data.main.pressure} hPa"
+                        tvUpdateTime.text ="Last Update: ${SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(data.dt * 1000)} hPa"
+                    }
                 }
             }
 
